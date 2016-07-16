21548fd0f7ace32c66ba5af2cfd2db88f89034c2
Final Calibur <fred11212@hotmail.com>
2016-07-16 17:32:12 -0400

    Added the Worker class to the framework

---
+++ src/viking/framework/worker/Worker.java
@@ -0,0 +1,40 @@
+package viking.framework.worker;
+
+import viking.framework.mission.Mission;
+
+/**
+ * This class represents a specific "task" to execute in a certain script.
+ * For example, it could represent "chop" in a WC script, or "fish" in a Fishing
+ * script.
+ * 
+ * @author The Viking
+ *
+ * @param <T> The mission we're working for
+ */
+public abstract class Worker<T extends Mission>
+{
+	protected T mission;
+	
+	/**
+	 * Primary constructor for the Worker class. We supply
+	 * it with a mission, so that it can access any data it needs
+	 * 
+	 * @param mission the mission that is driving this worker
+	 */
+	public Worker(T mission)
+	{
+		this.mission = mission;
+	}
+	
+	/**
+	 * This method tells us whether or not we need to repeat execution of this worker
+	 * 
+	 * @return true if we need to repeat execution of this worker, false if we should go back to the root
+	 */
+	public abstract boolean needsRepeat();
+	
+	/**
+	 * This method handles the execution of this worker
+	 */
+	public abstract void work();
+}
