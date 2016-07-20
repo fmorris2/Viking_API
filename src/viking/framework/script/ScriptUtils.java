398f64caa6b94ed69c5f5a767a34aec452172434
Final Calibur <fred11212@hotmail.com>
2016-07-20 15:44:40 -0400

    filters is now instantiated in the ScriptUtils class

--- src/viking/framework/script/ScriptUtils.java
+++ src/viking/framework/script/ScriptUtils.java
@@ -24,6 +24,7 @@ public class ScriptUtils
 		bank = new BankUtils(script);
 		walk = new WalkingUtils(script);
 		conditions = new VConditions(script);
+		filters = new VFilters(script);
 	}
 	
 	//Getters
