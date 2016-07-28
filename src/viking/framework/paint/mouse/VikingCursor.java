502466743b850ad9f57bab9447d3cc57e9c5a109
Final Calibur <fred11212@hotmail.com>
2016-07-28 19:41:15 -0400

    Minor change to Viking cursor offset

--- src/viking/framework/paint/mouse/VikingCursor.java
+++ src/viking/framework/paint/mouse/VikingCursor.java
@@ -8,8 +8,8 @@ import viking.framework.paint.image.VImage;
 public class VikingCursor
 {
 	private static final String CURSOR_URL = "http://vikingscripts.io/script/cursor.png";
-	private static final int OFFSET_X = 16;
-	private static final int OFFSET_Y = 16;
+	private static final int OFFSET_X = 15;
+	private static final int OFFSET_Y = 15;
 	
 	private VImage cursorImage;
 	
