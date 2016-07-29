b1dc071a8da8e8620846f86dfff6bb6cb08840c1
Final Calibur <fred11212@hotmail.com>
2016-07-29 14:18:54 -0400

    Testing

--- src/viking/framework/paint/mouse/VikingCursor.java
+++ src/viking/framework/paint/mouse/VikingCursor.java
@@ -14,7 +14,7 @@ public class VikingCursor
 	private static final int OFFSET_X = 9;
 	private static final int OFFSET_Y = 9;
 	
-	private static final double ANGLE_CHANGE = 6;
+	private static final double ANGLE_CHANGE = 3;
 	
 	private VImage innerImage;
 	private VImage outerImage;
@@ -42,7 +42,7 @@ public class VikingCursor
 		final int X = mousePos.x - OFFSET_X;
 		final int Y = mousePos.y - OFFSET_Y;
 		
-		g.rotate(ROTATION, X/* + image.getImage().getWidth() / 2*/, Y /*+ image.getImage().getHeight() / 2*/);
+		g.rotate(ROTATION, mousePos.x + OFFSET_X/* + image.getImage().getWidth() / 2*/, mousePos.y + OFFSET_Y/*+ image.getImage().getHeight() / 2*/);
 		image.draw(g, X, Y);
 		g.setTransform(old);
 	}
