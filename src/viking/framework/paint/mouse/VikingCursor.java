1110353811b5235a838c250b19c9aec9181b376c
Final Calibur <fred11212@hotmail.com>
2016-07-29 11:56:31 -0400

    Started work on new cursor

--- src/viking/framework/paint/mouse/VikingCursor.java
+++ src/viking/framework/paint/mouse/VikingCursor.java
@@ -7,19 +7,24 @@ import viking.framework.paint.image.VImage;
 
 public class VikingCursor
 {
-	private static final String CURSOR_URL = "http://vikingscripts.io/script/cursor.png";
-	private static final int OFFSET_X = 15;
-	private static final int OFFSET_Y = 15;
+	private static final String INNER_CURSOR_URL = "http://vikingscripts.io/script/cursor_inner.png";
+	private static final String OUTER_CURSOR_URL = "http://vikingscripts.io/script/cursor_outer.png";
 	
-	private VImage cursorImage;
+	private static final int OFFSET_X = 9;
+	private static final int OFFSET_Y = 9;
+	
+	private VImage innerImage;
+	private VImage outerImage;
 	
 	public VikingCursor()
 	{
-		cursorImage = new VImage(CURSOR_URL);
+		innerImage = new VImage(INNER_CURSOR_URL);
+		outerImage = new VImage(OUTER_CURSOR_URL);
 	}
 	
 	public void draw(Graphics2D g, Point mousePos)
 	{
-		cursorImage.draw(g, mousePos.x - OFFSET_X, mousePos.y - OFFSET_Y);
+		innerImage.draw(g, mousePos.x - OFFSET_X, mousePos.y - OFFSET_Y);
+		outerImage.draw(g, mousePos.x - OFFSET_X, mousePos.y - OFFSET_Y);
 	}
 }
