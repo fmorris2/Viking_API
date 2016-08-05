78d9ec136583f06ab195fcd0d3dc861ca30d1764
Final Calibur <fred11212@hotmail.com>
2016-08-05 18:14:43 -0400

    Throttle was not a good idea

--- src/viking/framework/paint/plugin/impl/image/InteractableImagePlugin.java
+++ src/viking/framework/paint/plugin/impl/image/InteractableImagePlugin.java
@@ -4,16 +4,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-import viking.api.Timing;
 import viking.framework.paint.VikingPaint;
 import viking.framework.script.VikingScript;
 
 public abstract class InteractableImagePlugin extends ImagePlugin implements MouseListener, MouseMotionListener
 {
-	private static final int MOUSE_MOVE_THRESH = 500;
-	
 	protected boolean isMouseOnImage;
-	private long lastMouseMove;
 	
 	public InteractableImagePlugin(VikingScript script, VikingPaint<?> paint,
 			String imageUrl, int x, int y)
@@ -24,16 +20,11 @@ public abstract class InteractableImagePlugin extends ImagePlugin implements Mou
 	
 	@Override
 	public void mouseMoved(MouseEvent e)
-	{	
-		if(Timing.timeFromMark(lastMouseMove) < MOUSE_MOVE_THRESH)
-			return;
-		
+	{		
 		if(image.getBounds() != null && image.getBounds().contains(e.getPoint()))
 			isMouseOnImage = true;
 		else
 			isMouseOnImage = false;
-		
-		lastMouseMove = Timing.currentMs();
 	}
 	
 	@Override
