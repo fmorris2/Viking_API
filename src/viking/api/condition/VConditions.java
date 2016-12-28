ad9ed618e61e73c61aff0d85b6816f80f9e8d50f
Sphiinx <deathbyman33@yahoo.com>
2016-12-27 20:41:31 -0700

    Added isBankOpen to VConditions.java

--- src/viking/api/condition/VConditions.java
+++ src/viking/api/condition/VConditions.java
@@ -13,69 +13,65 @@ import viking.framework.script.VikingScript;
  * Condition code in the actual scripts, and also offers
  * a performance boost due to a lot of the conditions being
  * cached in constants
- * 
- * @author The Viking
  *
+ * @author The Viking
  */
-public class VConditions extends ScriptUtil
-{
-	public final VCondition IN_COMBAT = inCombat();
-	
-	public VConditions(VikingScript script)
-	{
-		super(script);
-	}
+public class VConditions extends ScriptUtil {
 
-	public VCondition inAreaCondition(Area a)
-	{
-		return new VCondition()
-		{
-			@Override
-			public boolean evaluate()
-			{
-				return a.contains(script.myPosition());
-			}
-			
-		};
-	}
-	
-	public VCondition stolenEntity(Character<?> e)
-	{
-		return new VCondition()
-		{
-			@Override
-			public boolean evaluate()
-			{
-				Character<?> interacting = e.getInteracting();
-				return interacting != null && !interacting.equals(myPlayer());
-			}
-			
-		};
-	}
-	
-	public VCondition onScreenCondition(Entity e)
-	{
-		return new VCondition()
-		{
-			@Override
-			public boolean evaluate()
-			{
-				return e == null || e.isVisible();
-			}
-			
-		};
-	}
-	
-	private VCondition inCombat()
-	{
-		return new VCondition()
-		{
-			@Override
-			public boolean evaluate()
-			{
-				return combat.isFighting() || myPlayer().isUnderAttack();
-			}
-			
-		};
-	}
+    public final VCondition IN_COMBAT = inCombat();
+    public final VCondition BANK_OPEN = isBankOpen();
+
+    public VConditions(VikingScript script) {
+        super(script);
+    }
+
+    public VCondition inAreaCondition(Area a) {
+        return new VCondition() {
+            @Override
+            public boolean evaluate() {
+                return a.contains(script.myPosition());
+            }
+
+        };
+    }
+
+    public VCondition stolenEntity(Character<?> e) {
+        return new VCondition() {
+            @Override
+            public boolean evaluate() {
+                Character<?> interacting = e.getInteracting();
+                return interacting != null && !interacting.equals(myPlayer());
+            }
+
+        };
+    }
+
+    public VCondition onScreenCondition(Entity e) {
+        return new VCondition() {
+            @Override
+            public boolean evaluate() {
+                return e == null || e.isVisible();
+            }
+
+        };
+    }
+
+    private VCondition inCombat() {
+        return new VCondition() {
+            @Override
+            public boolean evaluate() {
+                return combat.isFighting() || myPlayer().isUnderAttack();
+            }
+        };
+    }
+
+    private VCondition isBankOpen() {
+        return new VCondition() {
+            @Override
+            public boolean evaluate() {
+                return bank.isOpen();
+            }
+        };
+    }
+
 }
