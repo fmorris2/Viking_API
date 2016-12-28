a3200314bf1d18d78514135b37693c9fbe6cd299
Dad <Dad>
2016-12-27 23:18:52 -0500

    Updates to task framework

--- src/viking/framework/task/Task.java
+++ src/viking/framework/task/Task.java
@@ -1,17 +1,19 @@
 package viking.framework.task;
 
+import viking.framework.VMethodProvider;
 import viking.framework.mission.Mission;
 
 /**
  * Created by Sphiinx on 4/20/2016.
  */
-public abstract class Task<T extends Mission> {
+public abstract class Task<T extends Mission> extends VMethodProvider {
 	
 	protected T mission;
 	
 	public Task(T mission)
 	{
 		this.mission = mission;
+		exchangeContext(mission.getScript());
 	}
 	
     abstract boolean validate();
