3c742f568296af4b10a475f17aa7615e4e4e813b
Sphiinx <deathbyman33@yahoo.com>
2016-12-31 23:58:56 -0700

    Updated VikingBank,java: Added all the currently mapped OSBot banks.

--- src/viking/api/banking/VikingBank.java
+++ src/viking/api/banking/VikingBank.java
@@ -5,53 +5,56 @@ import org.osbot.rs07.api.map.constants.Banks;
 
 /**
  * This is essentially a wrapper enum for the provided Bank constants in the
- * API. This enum allows for us to do things such as find the closest bank 
+ * API. This enum allows for us to do things such as find the closest bank
  * to our character, and add other filtering options such as whether or not
  * the bank is members only.
- * 
- * @author The Viking
  *
+ * @author The Viking
  */
-enum VikingBank
-{
-	AL_KHARID(Banks.AL_KHARID, false),
-	ARDOUGNE_NORTH(Banks.ARDOUGNE_NORTH, true),
-	ARDOUGNE_SOUTH(Banks.ARDOUGNE_SOUTH, true),
-	CAMELOT(Banks.CAMELOT, true),
-	CANIFIS(Banks.CANIFIS, true),
-	CASTLE_WARS(Banks.CASTLE_WARS, true),
-	CATHERBY(Banks.CATHERBY, true),
-	DRAYNOR(Banks.DRAYNOR, false),
-	DUEL_ARENA(Banks.DUEL_ARENA, false),
-	EDGEVILLE(Banks.EDGEVILLE, false),
-	FALADOR_EAST(Banks.FALADOR_EAST, false),
-	FALADOR_WEST(Banks.FALADOR_WEST, false),
-	GNOME_STRONGHOLD(Banks.GNOME_STRONGHOLD, true),
-	LUMBRIDGE_LOWER(Banks.LUMBRIDGE_LOWER, true),
-	LUMBRIDGE_UPPER(Banks.LUMBRIDGE_UPPER, false),
-	PEST_CONTROL(Banks.PEST_CONTROL, true),
-	TZHAAR(Banks.TZHAAR, true),
-	VARROCK_EAST(Banks.VARROCK_EAST, false),
-	VARROCK_WEST(Banks.VARROCK_WEST, false),
-	YANILLE(Banks.YANILLE, true);
-	
-	private Area area;
-	private boolean isMembers;
-	
-	VikingBank(Area area, boolean isMembers)
-	{
-		this.area = area;
-		this.isMembers = isMembers;
-	}
-	
-	//Getters & Setters
-	public Area getArea()
-	{
-		return area;
-	}
-	
-	public boolean isMembers()
-	{
-		return isMembers;
-	}
+public enum VikingBank {
+
+    YANILLE(Banks.YANILLE, true),
+    LUMBRIDGE_UPPER(Banks.LUMBRIDGE_UPPER, false),
+    TZHAAR(Banks.TZHAAR, true),
+    LOVAKENGJ_HOUSE(Banks.LOVAKENGJ_HOUSE, true),
+    VARROCK_WEST(Banks.VARROCK_WEST, false),
+    DUEL_ARENA(Banks.DUEL_ARENA, false),
+    AL_KHARID(Banks.AL_KHARID, false),
+    CASTLE_WARS(Banks.CASTLE_WARS, true),
+    EDGEVILLE(Banks.EDGEVILLE, false),
+    FALADOR_EAST(Banks.FALADOR_EAST, false),
+    ARDOUGNE_NORTH(Banks.ARDOUGNE_NORTH, true),
+    CATHERBY(Banks.CATHERBY, true),
+    PEST_CONTROL(Banks.PEST_CONTROL, true),
+    FALADOR_WEST(Banks.FALADOR_WEST, false),
+    GRAND_EXCHANGE(Banks.GRAND_EXCHANGE, false),
+    HOSIDIUS_HOUSE(Banks.HOSIDIUS_HOUSE, true),
+    SHAYZIEN_HOUSE(Banks.SHAYZIEN_HOUSE, true),
+    ARCEUUS_HOUSE(Banks.ARCEUUS_HOUSE, true),
+    GNOME_STRONGHOLD(Banks.GNOME_STRONGHOLD, true),
+    ARDOUGNE_SOUTH(Banks.ARDOUGNE_SOUTH, true),
+    VARROCK_EAST(Banks.VARROCK_EAST, false),
+    LUMBRIDGE_LOWER(Banks.LUMBRIDGE_LOWER, true),
+    DRAYNOR(Banks.DRAYNOR, false),
+    CAMELOT(Banks.CAMELOT, true),
+    CANIFIS(Banks.CANIFIS, true),
+    PISCARILIUS_HOUSE(Banks.PISCARILIUS_HOUSE, true),
+    LOVAKITE_MINE(Banks.LOVAKITE_MINE, true);
+
+    private Area area;
+    private boolean isMembers;
+
+    VikingBank(Area area, boolean isMembers) {
+        this.area = area;
+        this.isMembers = isMembers;
+    }
+
+    //Getters & Setters
+    public Area getArea() {
+        return area;
+    }
+
+    public boolean isMembers() {
+        return isMembers;
+    }
 }
