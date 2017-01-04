293b11b198e142baf23d9388b77a593feccf0a18
Sphiinx <deathbyman33@yahoo.com>
2017-01-03 19:39:13 -0700

    Finished fishing api

---
+++ src/viking/api/skills/fishing/enums/FishType.java
@@ -0,0 +1,24 @@
+package viking.api.skills.fishing.enums;
+
+/**
+ * Created by Sphiinx on 1/3/2017.
+ */
+public enum FishType {
+
+    SHRIMP(317, "Net", new FishingEquipment[]{
+            FishingEquipment.SMALL_FISHING_NET
+    }, FishingLocation.LUMBRIDGE);
+
+    public int ITEM_ID;
+    public String FISH_ACTION;
+    public FishingEquipment[] REQUIRED_EQUIPMENT;
+    public FishingLocation[] FISHING_LOCATIONS;
+
+    FishType(int item_id, String fish_action, FishingEquipment[] required_equipment, FishingLocation... fishing_locations) {
+        this.ITEM_ID = item_id;
+        this.FISH_ACTION = fish_action;
+        this.REQUIRED_EQUIPMENT = required_equipment;
+        this.FISHING_LOCATIONS = fishing_locations;
+    }
+
+}
---
+++ src/viking/api/skills/fishing/enums/FishingLocation.java
@@ -0,0 +1,39 @@
+package viking.api.skills.fishing.enums;
+
+import org.osbot.rs07.api.map.Area;
+
+/**
+ * Created by Sphiinx on 1/3/2017.
+ */
+public enum FishingLocation {
+
+    LUMBRIDGE(new Area(new int[][]{
+                    { 3250, 3165 },
+                    { 3243, 3139 },
+                    { 3233, 3138 },
+                    { 3243, 3162 }
+            }), false, FishType.SHRIMP);
+
+    private final Area AREA;
+    private final boolean MEMBERS;
+    private final FishType FISH_TYPE[];
+
+    FishingLocation(Area area, boolean members, FishType... tree_type) {
+        this.AREA = area;
+        this.MEMBERS = members;
+        this.FISH_TYPE = tree_type;
+    }
+
+    public Area getArea() {
+        return AREA;
+    }
+
+    public boolean isMembers() {
+        return MEMBERS;
+    }
+
+    public FishType[] getTreeTypes() {
+        return FISH_TYPE;
+    }
+
+}
