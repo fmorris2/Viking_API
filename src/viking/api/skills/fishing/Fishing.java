983c1b3fb1e972de8a98e4279770628870a3d4b8
Sphiinx <deathbyman33@yahoo.com>
2017-01-03 23:40:30 -0700

    Finished gathering fishing data

--- src/viking/api/skills/fishing/Fishing.java
+++ src/viking/api/skills/fishing/Fishing.java
@@ -1,10 +1,33 @@
 package viking.api.skills.fishing;
 
+import org.osbot.rs07.api.ui.Skill;
+import viking.api.skills.fishing.enums.FishType;
+import viking.framework.VMethodProvider;
+
 /**
  * Created by Sphiinx on 1/3/2017.
  */
-public class Fishing {
+public class Fishing extends VMethodProvider {
 
+    /**
+     * Gets the highest level fish that the RSPlayer can fish.
+     *
+     * @return The highest level tree the RSPlayer can chop
+     */
+    public FishType getBestFishableFish(boolean is_members) {
+        final FishType[] tree_types = FishType.values();
+
+        for (int i = tree_types.length - 1; i >= 0; i--) {
+            if (tree_types[i].getFishingLevel() <= skills.getStatic(Skill.FISHING)) {
+                if (tree_types[i].isMembers() && !is_members) {
+                    continue;
+                }
+                return tree_types[i];
+            }
+        }
+
+        return FishType.SHRIMP;
+    }
 
 }
 
--- src/viking/api/skills/fishing/enums/FishType.java
+++ src/viking/api/skills/fishing/enums/FishType.java
@@ -7,17 +7,54 @@ import java.util.Arrays;
  */
 public enum FishType {
 
-    SHRIMP(317, "Net", new FishingEquipment[]{
+    SHRIMP(317, 1, false, "Net", new FishingEquipment[]{
             FishingEquipment.SMALL_FISHING_NET
-    }, FishingLocation.LUMBRIDGE);
+    }, FishingLocation.F_LUMBRIDGE_SOUTH_1, FishingLocation.F_AL_KHARID_1, FishingLocation.F_KARAMJA_1, FishingLocation.F_MUDSKIPPER_POINT_1, FishingLocation.F_DRAYNOR_VILLAGE_1),
+    SARDINE(327, 5, false, "Bait", new FishingEquipment[]{
+            FishingEquipment.FISHING_BAIT,
+            FishingEquipment.FISHING_ROD
+    }, FishingLocation.F_LUMBRIDGE_SOUTH_1, FishingLocation.F_AL_KHARID_1, FishingLocation.F_KARAMJA_1, FishingLocation.F_MUDSKIPPER_POINT_1, FishingLocation.F_DRAYNOR_VILLAGE_1),
+    HERRING(345, 10, false, "Bait", new FishingEquipment[]{
+            FishingEquipment.FISHING_BAIT,
+            FishingEquipment.FISHING_ROD
+    }, FishingLocation.F_LUMBRIDGE_SOUTH_1, FishingLocation.F_AL_KHARID_1, FishingLocation.F_KARAMJA_1, FishingLocation.F_MUDSKIPPER_POINT_1, FishingLocation.F_DRAYNOR_VILLAGE_1),
+    ANCHOVIES(321, 15, false, "Net", new FishingEquipment[]{
+            FishingEquipment.SMALL_FISHING_NET
+    }, FishingLocation.F_LUMBRIDGE_SOUTH_1, FishingLocation.F_AL_KHARID_1, FishingLocation.F_KARAMJA_1, FishingLocation.F_MUDSKIPPER_POINT_1, FishingLocation.F_DRAYNOR_VILLAGE_1),
+    TROUT(335, 20, false, "Lure", new FishingEquipment[]{
+            FishingEquipment.FLY_FISHING_ROD,
+            FishingEquipment.FEATHER,
+    }, FishingLocation.F_LUMBRIDGE_NORTH_1, FishingLocation.F_BARBARIAN_VILLAGE_1),
+    PIKE(349, 25, false, "Bait", new FishingEquipment[]{
+            FishingEquipment.FISHING_ROD,
+            FishingEquipment.FISHING_BAIT,
+    }, FishingLocation.F_LUMBRIDGE_NORTH_1, FishingLocation.F_BARBARIAN_VILLAGE_1),
+    SALMON(331, 30, false, "Lure", new FishingEquipment[]{
+            FishingEquipment.FLY_FISHING_ROD,
+            FishingEquipment.FEATHER,
+    }, FishingLocation.F_LUMBRIDGE_NORTH_1, FishingLocation.F_BARBARIAN_VILLAGE_1),
+    TUNA(359, 35, false, "Harpoon", new FishingEquipment[]{
+            FishingEquipment.HARPOON,
+    }, FishingLocation.F_KARAMJA_1),
+    LOBSTER(377, 40, false, "Cage", new FishingEquipment[]{
+            FishingEquipment.LOBSTER_POT
+    }, FishingLocation.F_KARAMJA_1),
+    SWORDFISH(371, 50, false, "Harpoon", new FishingEquipment[]{
+            FishingEquipment.HARPOON,
+    }, FishingLocation.F_KARAMJA_1);
+
 
     public int ITEM_ID;
+    public int FISHING_LEVEL;
+    public boolean IS_MEMBERS;
     public String FISH_ACTION;
     public FishingEquipment[] REQUIRED_EQUIPMENT;
     public FishingLocation[] FISHING_LOCATIONS;
 
-    FishType(int item_id, String fish_action, FishingEquipment[] required_equipment, FishingLocation... fishing_locations) {
+    FishType(int item_id, int fishing_level, boolean is_members, String fish_action, FishingEquipment[] required_equipment, FishingLocation... fishing_locations) {
         this.ITEM_ID = item_id;
+        this.FISHING_LEVEL = fishing_level;
+        this.IS_MEMBERS = is_members;
         this.FISH_ACTION = fish_action;
         this.REQUIRED_EQUIPMENT = required_equipment;
         this.FISHING_LOCATIONS = fishing_locations;
@@ -25,6 +62,14 @@ public enum FishType {
 
     public int getItemID() {
         return ITEM_ID;
+    }
+
+    public int getFishingLevel() {
+        return FISHING_LEVEL;
+    }
+
+    public boolean isMembers() {
+        return this.IS_MEMBERS;
     }
 
     public String getFishingAction() {
@@ -43,4 +88,4 @@ public enum FishType {
         return Arrays.stream(REQUIRED_EQUIPMENT).mapToInt(FishingEquipment::getItemID).toArray();
     }
 
-}
+    }
