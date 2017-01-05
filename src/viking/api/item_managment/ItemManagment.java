4fc1be7e9d0b53111a49de362ca3564215d21a4c
Sphiinx <deathbyman33@yahoo.com>
2017-01-04 22:52:35 -0700

    Changed ItemManagment ArrayLists to int arrays

--- src/viking/api/item_managment/ItemManagment.java
+++ src/viking/api/item_managment/ItemManagment.java
@@ -1,9 +1,6 @@
 package viking.api.item_managment;
 
-import org.osbot.rs07.api.model.Item;
 import viking.framework.VMethodProvider;
-
-import java.util.ArrayList;
 
 /**
  * Created by Sphiinx on 1/4/2017.
@@ -13,12 +10,12 @@ public class ItemManagment extends VMethodProvider {
     /**
      * ArrayList containing all of the itemIDs we should try to buy.
      */
-    public ArrayList<Integer> items_needed = new ArrayList<>();
+    public int[] items_needed;
 
     /**
      * ArrayList containing all of the itemIDs we're willing to sell in order to buy the items_needed.
      */
-    public ArrayList<Integer> items_willing_to_sell = new ArrayList<>();
+    public int[] items_willing_to_sell;
 
 
 }
