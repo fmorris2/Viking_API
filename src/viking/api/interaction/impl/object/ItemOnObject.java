e94140ca999c644cfd31700ecd5fda780916efdd
Dad <Dad>
2017-01-06 10:46:04 -0500

    D

--- src/viking/api/interaction/impl/object/ItemOnObject.java
+++ src/viking/api/interaction/impl/object/ItemOnObject.java
@@ -21,8 +21,11 @@ public class ItemOnObject extends EntityInteraction<RS2Object>
 	protected boolean interact()
 	{
 		Item item = vmp.inventory.getItem(action);
+		vmp.log("Search for item in inventory");
 		if(item == null)
 			return false;
+		
+		vmp.log("Item is not null");
 		
 		String selectedItem = vmp.inventory.getSelectedItemName();
 		
