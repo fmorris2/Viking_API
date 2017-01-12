f1f33b2cd58928f2968383ab9f58532eb4bac84d
Dad <Dad>
2017-01-12 16:45:04 -0500

    Added Orion Sell Items constant in ItemManagement

--- src/viking/framework/item_management/ItemManagement.java
+++ src/viking/framework/item_management/ItemManagement.java
@@ -1,7 +1,20 @@
 package viking.framework.item_management;
 
+import viking.api.skills.fishing.enums.FishType;
+import viking.api.skills.mining.enums.OreType;
+import viking.api.skills.woodcutting.enums.LogType;
+
 public interface ItemManagement
 {
+	public static final int[] ORION_SELL_ITEMS = 
+	{
+		LogType.NORMAL.getItemID(), LogType.OAK.getItemID(), LogType.WILLOW.getItemID(),
+		FishType.SHRIMP.getItemID(), FishType.SARDINE.getItemID(), FishType.HERRING.getItemID(),
+		FishType.ANCHOVIES.getItemID(), FishType.TROUT.getItemID(), FishType.PIKE.getItemID(),
+		FishType.SALMON.getItemID(), OreType.RUNE_ESSENCE.getItemID(), OreType.CLAY.getItemID(),
+		OreType.COPPER_ORE.getItemID(), OreType.TIN_ORE.getItemID(), OreType.IRON_ORE.getItemID()
+	};
+	
 	public IMEntry[] itemsToBuy();
 	public int[] itemsToSell();
 }
