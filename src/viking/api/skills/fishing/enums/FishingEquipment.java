9a58b5c2cb8159cdc1744d02d68a2ea29233f1e4
Sphiinx <deathbyman33@yahoo.com>
2017-01-13 22:27:12 -0700

    Added FishingEquipment.java levels

--- src/viking/api/skills/fishing/enums/FishingEquipment.java
+++ src/viking/api/skills/fishing/enums/FishingEquipment.java
@@ -7,25 +7,31 @@ import java.util.Arrays;
  */
 public enum FishingEquipment {
 
-    FISHING_ROD(307, 1),
-    FLY_FISHING_ROD(309, 1),
-    SMALL_FISHING_NET(303, 1),
-    BIG_FISHING_NET(-1, 1),
-    HARPOON(311, 1),
-    LOBSTER_POT(301, 1),
-    FEATHER(314, 0),
-    FISHING_BAIT(313, 0);
+    FISHING_ROD(307, 5, 1),
+    FLY_FISHING_ROD(309, 20, 1),
+    SMALL_FISHING_NET(303, 1, 1),
+    BIG_FISHING_NET(-1, -1, 1),
+    HARPOON(311, 35, 1),
+    LOBSTER_POT(301, 40, 1),
+    FEATHER(314, 20, -1),
+    FISHING_BAIT(313, 5, -1);
 
     private final int ITEM_ID;
+    private final int FISHING_LEVEL;
     private final int AMOUNT;
 
-    FishingEquipment(int item_id, int amount) {
+    FishingEquipment(int item_id, int fishing_level, int amount) {
         this.ITEM_ID = item_id;
+        this.FISHING_LEVEL = fishing_level;
         this.AMOUNT = amount;
     }
 
     public int getItemID() {
         return ITEM_ID;
+    }
+
+    public int getFishingLevel() {
+        return this.FISHING_LEVEL;
     }
 
     public int getItemAmount() {
