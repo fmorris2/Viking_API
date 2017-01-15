a33cf79422217291b1b3c744b96f950cb73abd72
Dad <Dad>
2017-01-14 21:09:24 -0500

    Almost done with muling

--- src/viking/framework/mule/MuleOrderEvent.java
+++ src/viking/framework/mule/MuleOrderEvent.java
@@ -1,16 +1,20 @@
 package viking.framework.mule;
 
+import org.osbot.rs07.api.Bank.BankMode;
 import org.osbot.rs07.api.map.Position;
 
+import viking.api.Timing;
 import viking.framework.script.VikingScript;
 
 public class MuleOrderEvent
 {
+	private static final int MULE_DIST_THRESH = 10;
+	
 	private VikingScript script;
 	private MuleOrder order;
 	private Position mulePos;
 	private String muleName;
-	private boolean hasFinished;
+	private boolean hasFinished, hasWalkedToMule, hasWithdrawnOrder;
 	
 	public MuleOrderEvent(VikingScript s, MuleOrder o)
 	{
@@ -25,9 +29,52 @@ public class MuleOrderEvent
 		if(muleName != null && mulePos != null)	
 		{
 			script.log(this, false, "Has mule info...");
+			if(!hasWalkedToMule && walkToMule())
+				hasWalkedToMule = true;
+			
+			if(hasWalkedToMule)
+			{
+				if(!hasWithdrawnOrder)
+					withdrawOrder();
+				else if(walkToMule())
+					tradeMule();
+			}
+			
+			
+				
 		}
 		else
 			script.log(this, false, "Mule info has not been provided yet...");
+	}
+	
+	private void tradeMule()
+	{
+		script.log(this, false , "trade mule");
+	}
+	
+	private boolean walkToMule()
+	{
+		if(script.myPosition().distance(mulePos) <= MULE_DIST_THRESH)
+			return true;
+		
+		return script.getUtils().walk.walkTo(mulePos)
+					&& Timing.waitCondition(() -> script.myPosition().distance(mulePos) <= MULE_DIST_THRESH, 3500);
+	}
+	
+	private void withdrawOrder()
+	{
+		if(!script.getUtils().bank.isInBank())
+			script.getWalking().webWalk(script.getUtils().bank.getAllBanks(false, false));
+		else if(!script.getBank().isOpen())
+			script.getUtils().bank.open();
+		else if(script.bank.enableMode(BankMode.WITHDRAW_NOTE))
+		{
+			for(int i : order.ITEMS)
+				if(script.bank.contains(i) && !script.bank.withdrawAll(i))
+					break;
+			
+			hasWithdrawnOrder = true;
+		}
 	}
 	
 	public boolean shouldExecute()
