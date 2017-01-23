2a38a60df29177f6fd2348c2c2c15c6097681993
Sphiinx <deathbyman33@yahoo.com>
2017-01-23 01:23:14 -0700

    Error fixes

--- src/viking/framework/item_management/ItemManagement.java
+++ src/viking/framework/item_management/ItemManagement.java
@@ -1,22 +1,22 @@
 package viking.framework.item_management;
 
 import viking.api.skills.fishing.enums.FishType;
-import viking.api.skills.mining.enums.OreType;
+import viking.api.skills.mining.enums.RockType;
 import viking.api.skills.woodcutting.enums.LogType;
 
-public interface ItemManagement
-{
-	public static final int[] ORION_SELL_ITEMS = 
-	{
-		LogType.NORMAL.getItemID(), LogType.OAK.getItemID(), LogType.WILLOW.getItemID(),
-		FishType.SHRIMP.getItemID(), FishType.SARDINE.getItemID(), FishType.HERRING.getItemID(),
-		FishType.ANCHOVIES.getItemID(), FishType.TROUT.getItemID(), FishType.PIKE.getItemID(),
-		FishType.SALMON.getItemID(), OreType.RUNE_ESSENCE.getItemID(), OreType.CLAY.getItemID(),
-		OreType.COPPER_ORE.getItemID(), OreType.TIN_ORE.getItemID(), OreType.IRON_ORE.getItemID(),
-		526, 1739, 314
-	};
-	//bones, cowhide, feather
-	
-	public IMEntry[] itemsToBuy();
-	public int[] itemsToSell();
+public interface ItemManagement {
+
+    int[] ORION_SELL_ITEMS = {
+            LogType.NORMAL.getItemID(), LogType.OAK.getItemID(), LogType.WILLOW.getItemID(),
+            FishType.SHRIMP.getItemID(), FishType.SARDINE.getItemID(), FishType.HERRING.getItemID(),
+            FishType.ANCHOVIES.getItemID(), FishType.TROUT.getItemID(), FishType.PIKE.getItemID(),
+            FishType.SALMON.getItemID(), RockType.RUNE_ESSENCE.getItemID(), RockType.CLAY.getItemID(),
+            RockType.COPPER_ORE.getItemID(), RockType.TIN_ORE.getItemID(), RockType.IRON_ORE.getItemID(),
+            526, 1739, 314
+    };
+    //bones, cowhide, feather
+
+    public IMEntry[] itemsToBuy();
+
+    public int[] itemsToSell();
 }
