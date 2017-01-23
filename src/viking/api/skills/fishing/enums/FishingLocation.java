b4093e3c97b335be051e07871f21c6ea0242b99b
Sphiinx <deathbyman33@yahoo.com>
2017-01-23 01:00:33 -0700

    Added MiningLocation.java, added reccomended combat levels to locations

--- src/viking/api/skills/fishing/enums/FishingLocation.java
+++ src/viking/api/skills/fishing/enums/FishingLocation.java
@@ -12,53 +12,55 @@ public enum FishingLocation {
             {3243, 3139},
             {3233, 3138},
             {3243, 3162}
-    }), false, false, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
+    }), false, false, -1, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
     F_AL_KHARID_1(new Area(new int[][]{
             {3254, 3151},
             {3264, 3158},
             {3287, 3137},
             {3277, 3131}
-    }), false, false, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
+    }), false, false, 29, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
     F_LUMBRIDGE_NORTH_1(new Area(new int[][]{
             {3237, 3227},
             {3249, 3227},
             {3242, 3259},
             {3231, 3259}
-    }), false, false, FishType.PIKE, FishType.SALMON, FishType.TROUT),
+    }), false, false, -1, FishType.PIKE, FishType.SALMON, FishType.TROUT),
     F_BARBARIAN_VILLAGE_1(new Area(new int[][]{
             {3101, 3440},
             {3118, 3439},
             {3116, 3412},
             {3100, 3413}
-    }), false, false, FishType.PIKE, FishType.SALMON, FishType.TROUT),
+    }), false, false, -1, FishType.PIKE, FishType.SALMON, FishType.TROUT),
     F_DRAYNOR_VILLAGE_1(new Area(new int[][]{
             {3072, 3237},
             {3077, 3243},
             {3094, 3228},
             {3085, 3219}
-    }), false, false, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
+    }), false, false, 15, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES),
     F_KARAMJA_1(new Area(new int[][]{
             {2916, 3192},
             {2938, 3190},
             {2936, 3165},
             {2909, 3177}
-    }), false, true, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES, FishType.TUNA, FishType.LOBSTER, FishType.SWORDFISH),
+    }), false, true, -1, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES, FishType.TUNA, FishType.LOBSTER, FishType.SWORDFISH),
     F_MUDSKIPPER_POINT_1(new Area(new int[][]{
             {2976, 3180},
             {2991, 3186},
             {3004, 3158},
             {2991, 3148}
-    }), false, true, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES);
+    }), false, true, -1, FishType.SHRIMP, FishType.SARDINE, FishType.HERRING, FishType.ANCHOVIES);
 
     private final Area AREA;
     private final boolean MEMBERS;
     private final boolean SHOULD_USE_DEPOSIT_BOX;
+    private final int RECOMMENDED_COMBAT_LEVEL;
     private final FishType FISH_TYPE[];
 
-    FishingLocation(Area area, boolean members, boolean use_deposit_box, FishType... tree_type) {
+    FishingLocation(Area area, boolean members, boolean use_deposit_box, int recommended_combat_level, FishType... tree_type) {
         this.AREA = area;
         this.MEMBERS = members;
         this.SHOULD_USE_DEPOSIT_BOX = use_deposit_box;
+        this.RECOMMENDED_COMBAT_LEVEL = recommended_combat_level;
         this.FISH_TYPE = tree_type;
     }
 
@@ -72,6 +74,10 @@ public enum FishingLocation {
 
     public boolean shouldUseDepositBox() {
         return SHOULD_USE_DEPOSIT_BOX;
+    }
+
+    public int getRecommendedCombatLevel() {
+        return RECOMMENDED_COMBAT_LEVEL;
     }
 
     public FishType[] getFishTypes() {
--- src/viking/api/skills/mining/enums/RockType.java
+++ src/viking/api/skills/mining/enums/RockType.java
@@ -5,32 +5,38 @@ package viking.api.skills.mining.enums;
  */
 public enum RockType {
 
-    RUNE_ESSENCE(-1, false, 1),
-    CLAY(6705, false, 1),
-    COPPER_ORE(4645, false, 1),
-    TIN_ORE(53, false, 1),
-    LIMESTONE(10295, true, 10),
-    IRON_ORE(2576, false, 15),
-    SILVER_ORE(74, false, 20),
-    COAL(10508, false, 30),
-    PURE_ESSENCE(-1, true, 30),
-    GOLD(8885, false, 40),
-    MITHRIL(-22239, false, 55),
-    ADAMANTITE(21662, false, 70),
-    RUNITE(-1, false, 85);
+    RUNE_ESSENCE(-1, 1436, false, 1),
+    CLAY(6705, 434, false, 1),
+    COPPER_ORE(4645, 436, false, 1),
+    TIN_ORE(53, 438, false, 1),
+    LIMESTONE(10295, -1, true, 10),
+    IRON_ORE(2576, 440, false, 15),
+    SILVER_ORE(74, 442, false, 20),
+    COAL(10508, 453, false, 30),
+    PURE_ESSENCE(-1, -1, true, 30),
+    GOLD(8885, 444, false, 40),
+    MITHRIL(-22239, 447, false, 55),
+    ADAMANTITE(21662, 449, false, 70),
+    RUNITE(-1, 451, false, 85);
 
     private final int COLOR;
+    private final int ITEM_ID;
     private final boolean IS_MEMBERS;
     private final int LEVEL;
 
-    RockType(int color, boolean is_members, int level) {
+    RockType(int color, int item_id, boolean is_members, int level) {
         this.COLOR = color;
+        this.ITEM_ID = item_id;
         this.IS_MEMBERS = is_members;
         this.LEVEL = level;
     }
 
     public short getRockColor() {
         return (short) COLOR;
+    }
+
+    public int getItemID() {
+        return ITEM_ID;
     }
 
     public boolean isMembers() {
