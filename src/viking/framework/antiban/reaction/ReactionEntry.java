73bdd3ab280b735a48ef72b9f5264d23b2ff7a52
Dad <Dad>
2017-02-18 01:28:34 -0500

    ReactionEntry changes

--- src/viking/framework/antiban/reaction/ReactionEntry.java
+++ src/viking/framework/antiban/reaction/ReactionEntry.java
@@ -5,15 +5,23 @@ import viking.api.Timing;
 public class ReactionEntry
 {
 	private long reactionStart, reactionEnd;
+	private boolean hasStarted;
 	
 	public void start()
 	{
+		hasStarted = true;
 		reactionStart = Timing.currentMs();
 	}
 	
 	public void end()
 	{
 		reactionEnd = Timing.currentMs();
+		hasStarted = false;
+	}
+	
+	public boolean hasStarted()
+	{
+		return hasStarted;
 	}
 	
 	public long getLength()
